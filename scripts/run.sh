03b1990ee9251eed83ade2b3ace2abea4f29ccc1
Sassan Haradji <me@sassanh.com>
2024-06-28 12:30:37 +0400

    feat: make run script verbose about what it's doing

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,6 +1,16 @@
 #!/usr/bin/env sh
 
+set -o errexit
+set -o pipefail
+set -o nounset
+set -o xtrace
+
 ssh ubo-development-pod "cat <<'EOF' > /tmp/run_script.sh
+set -o errexit
+set -o pipefail
+set -o nounset
+set -o xtrace
+
 cd ~/actions-runner
 PATH=\$PATH:~/.local/bin tmux new-session -d ./run.sh
 EOF
