322b17b347d6cac76bd85fc44b74db77b68fd099
Sassan Haradji <me@sassanh.com>
2024-06-28 12:28:46 +0400

    feat: make the setup script set the "ubo-pod" label

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -24,8 +24,7 @@ mkdir actions-runner && cd actions-runner
 curl -o actions-runner-linux-arm64-2.317.0.tar.gz -L https://github.com/actions/runner/releases/download/v2.317.0/actions-runner-linux-arm64-2.317.0.tar.gz
 echo '7e8e2095d2c30bbaa3d2ef03505622b883d9cb985add6596dbe2f234ece308f3  actions-runner-linux-arm64-2.317.0.tar.gz' | shasum -a 256 -c
 tar xzf ./actions-runner-linux-arm64-2.317.0.tar.gz
-./config.sh --url https://github.com/ubopod/ubo_app --token $GITHUB_TOKEN --unattended
-cat /tmp/get-poetry.py | python3 -
+./config.sh --url https://github.com/ubopod/ubo_app --token $GITHUB_TOKEN --unattended --labels ubo-pod
 EOF
 sudo -u ubo bash /tmp/run_script.sh
 rm -f /tmp/run_script.sh"
