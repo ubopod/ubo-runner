4706b4058ea15df15276ffe62d4613577098c2de
Sassan Haradji <me@sassanh.com>
2024-06-28 12:14:23 +0400

    fix: syntax error in the setup script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -14,9 +14,8 @@ mkdir actions-runner && cd actions-runner
 curl -o actions-runner-linux-arm64-2.317.0.tar.gz -L https://github.com/actions/runner/releases/download/v2.317.0/actions-runner-linux-arm64-2.317.0.tar.gz
 echo '7e8e2095d2c30bbaa3d2ef03505622b883d9cb985add6596dbe2f234ece308f3  actions-runner-linux-arm64-2.317.0.tar.gz' | shasum -a 256 -c
 tar xzf ./actions-runner-linux-arm64-2.317.0.tar.gz
-./config.sh --url https://github.com/ubopod/ubo_app --token $GITHUB_TOKEN --unattended"
+./config.sh --url https://github.com/ubopod/ubo_app --token $GITHUB_TOKEN --unattended
 cat /tmp/get-poetry.py | python3 -
 EOF
 sudo -u ubo bash /tmp/run_script.sh
 rm -f /tmp/run_script.sh"
-ssh ubo-development-pod sudo -u ubo bash -- -c "cd &&\
